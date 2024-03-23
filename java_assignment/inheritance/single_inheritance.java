feb78893bb0e494b950e60c7b84415642ca7a4a4
Mustack-Rabbi <mustackrabbi@gmail.com>
2024-03-23 20:32:44 +0600

    first commit

---
+++ java_assignment/inheritance/single_inheritance.java
@@ -0,0 +1,35 @@
+package java_assignment.inheritance;
+/**
+ * Father
+ */
+class Father {
+    int money_cash = 50000;
+    int money_bank = 200000;
+}
+
+/**
+ * Son
+ */
+class Son extends Father {
+    int total;
+    public  void totalproparty() {
+        total = money_cash + money_bank;
+        System.out.println("Total Money :" + total);  
+    }
+    
+}
+public class single_inheritance {
+    public static void main(String[] args) {
+        System.out.println("Assignment: Inheritence (Singel Inheritence)");
+        System.out.println("============================================");
+        System.out.println("Father >> Sone");
+        System.out.println("--------------------------------------------");
+
+        Father obj1 = new Father();
+        Son obj2 = new Son();
+
+        System.out.println("Father Cash amount: " + obj1.money_cash);
+        System.out.println("Father Bank amount: " + obj1.money_bank);
+        obj2.totalproparty();
+    }
+}
